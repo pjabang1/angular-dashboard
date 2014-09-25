a1fad5fa49430d4c76825532d890b3fc35eb449a
Pa Essa Jabang <paessa.jabang@hibu.com>
2014-09-25 14:37:43 +0100

    Added src files
    Signed-off-by: Pa Essa Jabang <paessa.jabang@hibu.com>

---
+++ src/js/dashboard/controllers/chart-line-ctrl.js
@@ -0,0 +1,20 @@
+angular.module("Dashboard").controller("ChartLineCtrl", ['$scope', '$timeout', function($scope, $timeout) {
+
+        $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+        $scope.series = ['Series A', 'Series B'];
+        $scope.data = [
+            [65, 59, 80, 81, 56, 55, 40],
+            [28, 48, 40, 19, 86, 27, 90]
+        ];
+        $scope.onClick = function(points, evt) {
+            console.log(points, evt);
+        };
+
+        // Simulate async data update
+        $timeout(function() {
+            $scope.data = [
+                [28, 48, 40, 19, 86, 27, 90],
+                [65, 59, 80, 81, 56, 55, 40]
+            ];
+        }, 3000);
+    }]);
