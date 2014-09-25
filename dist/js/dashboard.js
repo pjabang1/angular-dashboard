12ed9581cd113e07c2e6c4338d5fd95aff18b7d4
Pa Essa Jabang <paessa.jabang@hibu.com>
2014-09-25 14:33:41 +0100

    Dashboard : Added Chart.js
    Signed-off-by: Pa Essa Jabang <paessa.jabang@hibu.com>

---
+++ dist/js/dashboard.js
@@ -0,0 +1,130 @@
+(function(){ 
+angular.module('Dashboard', ['ui.bootstrap', 'ui.router', 'ngCookies', 'chart.js']);
+'use strict';
+
+/**
+ * Route configuration for the Dashboard module.
+ */
+angular.module('Dashboard').config(['$stateProvider', '$urlRouterProvider', 
+    function($stateProvider, $urlRouterProvider) {
+
+    // For unmatched routes
+    $urlRouterProvider.otherwise('/');
+
+    // Application routes
+    $stateProvider
+        .state('index', {
+            url: '/',
+            templateUrl: 'dashboard.html'
+        })
+        .state('tables', {
+            url: '/tables', 
+            templateUrl: 'tables.html'
+        });
+}]);
+
+/**
+ * Master Controller
+ */
+angular.module('Dashboard')
+    .controller('MasterCtrl', ['$scope', '$cookieStore', MasterCtrl]);
+
+function MasterCtrl($scope, $cookieStore) {
+    /**
+     * Sidebar Toggle & Cookie Control
+     *
+     */
+    var mobileView = 992;
+
+    $scope.getWidth = function() { return window.innerWidth; };
+
+    $scope.$watch($scope.getWidth, function(newValue, oldValue)
+    {
+        if(newValue >= mobileView)
+        {
+            if(angular.isDefined($cookieStore.get('toggle')))
+            {
+                if($cookieStore.get('toggle') == false)
+                {
+                    $scope.toggle = false;
+                }            
+                else
+                {
+                    $scope.toggle = true;
+                }
+            }
+            else 
+            {
+                $scope.toggle = true;
+            }
+        }
+        else
+        {
+            $scope.toggle = false;
+        }
+
+    });
+
+    $scope.toggleSidebar = function() 
+    {
+        $scope.toggle = ! $scope.toggle;
+
+        $cookieStore.put('toggle', $scope.toggle);
+    };
+
+    window.onresize = function() { $scope.$apply(); };
+}
+
+/**
+ * Alerts Controller
+ */
+angular.module('Dashboard').controller('AlertsCtrl', ['$scope', AlertsCtrl]);
+
+function AlertsCtrl($scope) {
+    $scope.alerts = [
+        { type: 'success', msg: 'Thanks for visiting! Feel free to create pull requests to improve the dashboard!' },
+        { type: 'danger', msg: 'Found a bug? Create an issue with as many details as you can.' }
+    ];
+
+    $scope.addAlert = function() {
+        $scope.alerts.push({msg: 'Another alert!'});
+    };
+
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
+}
+angular.module("Dashboard").controller("ChartLineCtrl", ['$scope', '$timeout', function ($scope, $timeout) {
+
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+
+  // Simulate async data update
+  $timeout(function () {
+    $scope.data = [
+      [28, 48, 40, 19, 86, 27, 90],
+      [65, 59, 80, 81, 56, 55, 40]
+    ];
+  }, 3000);
+}]);
+/**
+ * Loading Directive
+ * @see http://tobiasahlin.com/spinkit/
+ */
+angular.module('Dashboard').directive('rdLoading', rdLoading);
+
+function rdLoading () {
+    var directive = {
+        restrict: 'AE',
+        template: '<div class="loading"><div class="double-bounce1"></div><div class="double-bounce2"></div></div>'
+    };
+    return directive;
+};
+})();
